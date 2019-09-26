32153ce8330d6c38a45e9bfcc49e5c44758c7a93
Sergey Ivanenko <storm06kr@gmail.com>
2019-09-26 19:44:31 +0300

    Add Spring MVC and necessary folders and files

---
+++ FinalProject/src/main/java/finalProject/controller/HelloController.java
@@ -0,0 +1,16 @@
+package finalProject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = {"/", "/login"})
+public class HelloController {
+	@RequestMapping(method = RequestMethod.GET)
+	public String printHello(ModelMap model) {
+		model.addAttribute("message", "Hello Spring MVC!");
+		return "hello";
+	}
+}
